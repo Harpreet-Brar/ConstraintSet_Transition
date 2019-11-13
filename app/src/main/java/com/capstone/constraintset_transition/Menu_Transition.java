67d4dffb20cd6ebedf9a147fe53e4d7de08d0f9e
HARPREET SINGH BRAR <51008350+brarharry@users.noreply.github.com>
2019-11-13 11:01:10 -0800

    Menu Button Transition.

--- app/src/main/java/com/capstone/constraintset_transition/Menu_Transition.java
+++ app/src/main/java/com/capstone/constraintset_transition/Menu_Transition.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.transition.ChangeBounds;
 import android.transition.Transition;
 import android.transition.TransitionManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.Button;
@@ -46,5 +49,25 @@ public class Menu_Transition extends AppCompatActivity {
                 constraintSet.applyTo(constraint);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.back, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if(itemId == R.id.back)
+        {
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
